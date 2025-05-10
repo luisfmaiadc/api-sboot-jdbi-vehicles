ebe21c1d494895fb2b39300a295c952456d01ec3
luisfmaiadc <lf.mcosta23@gmail.com>
2025-05-10 11:43:40 -0300

    Criação das tabelas do banco de dados, adição das libs JDBI e OpenAPI e configuração dos contratos a serem cumpridos nas requisições POST base da aplicação.

---
+++ src/main/resources/db/migration/V1__create-tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE veiculo (
+    id INT AUTO_INCREMENT,
+    fabricante VARCHAR(75) NOT NULL,
+    modelo VARCHAR(75) NOT NULL,
+    placa CHAR(7) NOT NULL UNIQUE,
+    ano_fabricacao YEAR NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE manutencao (
+    id INT AUTO_INCREMENT,
+    id_veiculo INT NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    custo DECIMAL(10, 2) NOT NULL,
+    data DATE NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_veiculo) REFERENCES veiculo(id)
+);
