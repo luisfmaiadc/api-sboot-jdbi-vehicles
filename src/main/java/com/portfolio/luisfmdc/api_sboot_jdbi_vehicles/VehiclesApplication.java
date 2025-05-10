884083fd01c8417769d810cee36d00482bcd2f19
luisfmaiadc <lf.mcosta23@gmail.com>
2025-05-10 10:09:31 -0300

    Inicializando o projeto.

---
+++ src/main/java/com/portfolio/luisfmdc/api_sboot_jdbi_vehicles/VehiclesApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.api_sboot_jdbi_vehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/luisfmdc/api_sboot_jdbi_vehicles/VehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.api_sboot_jdbi_vehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
